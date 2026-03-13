61c9df0b219456f7ed5bb3b567bac2953961d2ad
RuelasLopezGuillermoAdiel <23308060610328@cetis61.edu.mx>
2026-03-13 08:59:05 -0500

    denuevo

--- main.py
+++ main.py
@@ -5,34 +5,28 @@ def main(page: ft.Page):
     page.bgcolor = ft.Colors.BLUE_100
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    
-    CorreoUS = ft.TextField(
-        label = "Escribe Tu Correo Gmail",
-        hint_text = "Escribe Tu Correo Gmail"
-    )
-    
-    ContraseñaUS = ft.TextField(
-        label = "Escribe Tu Contraseña",
-        hint_text = "Escribe Tu Contraseña"
-    )
+    USUARIO_VALIDO = "admin"
+    CONTRASENA_VALIDA ="1234"
     
     def login_click(e):
-        contraseña = txt_contraseñaUS.value
+        usuario = txt_usuario.value
+        contrasena = txt_contrasena.value
         
-        if usuario == USUARIO_VALIDO and contraseña == CONTRASEÑA_VALIDA:
+        if usuario == USUARIO_VALIDO and contrasena == CONTRASENA_VALIDA:
             page.show_dialog(ft.SnackBar(ft.Text("!Inicio de secion exitoso!")))
-            
-        else: page.show_dialog(ft.SnackBar(ft.Text("Usuario o Contraseña Incorrectos.")))
+        else: 
+            page.show_dialog(ft.SnackBar(ft.Text("Usuario o Contraseña Incorrectos.")))
         
-    def forgot_clicl(e):
-        page.show_dialog(ft.Snackbar(ft.Text("se han enviado instrucciones a tu correo electronico")))
+    def forgot_click(e):
+        page.show_dialog(ft.SnackBar(ft.Text("se han enviado instrucciones a tu correo electronico")))
         
-    txt_usuario=ft.TextField(lavel="Usuario", autofocus=True)
-    txt_contraseña=ft.TextField(label="Contraseña", password=Tr)
+    txt_usuario=ft.TextField(label="Correo", autofocus=True)
+    
+    txt_contrasena=ft.TextField(label="Contraseña", password=True)
     
     IniciarS = ft.Button(content="iniciar Sesion",
     width=200,
-        color=ft.Colors.BLACK,
+        on_click=login_click,
         bgcolor=ft.Colors.GREEN_200,
     
     )
@@ -47,5 +41,23 @@ def main(page: ft.Page):
         weight=ft.FontWeight.BOLD,
         text_align=ft.TextAlign.CENTER,
         ),
-        CorreoUS, ContraseñaUS, IniciarS, Registrarse)
+        txt_usuario, txt_contrasena, IniciarS, Registrarse)
+    
+    def mostrar_pantalla_principal():
+        page.clean()
+        
+        page.navigation_bar = ft.NavigationBar(
+            destinations=[
+                ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
+                ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Explorar"),
+                ft.NavigationBarDestination(icon=ft.Icons.PERSON, label="Perfil"),
+            ],
+            on_change=label e: print(f"Seleccionado: {e.control.selected_index}")
+        )
+    page.add(
+        ft.AppBar(
+            title
+        )
+    )
+    
 ft.app(target=main)
